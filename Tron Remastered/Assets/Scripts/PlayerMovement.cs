c2af4596eed63ff7ba1570a26f789660ef30f611
MatthieuL <61740378+Matthieu39@users.noreply.github.com>
2024-03-12 15:36:03 +0100

    moto force rampe

--- Tron Remastered/Assets/Scripts/PlayerMovement.cs
+++ Tron Remastered/Assets/Scripts/PlayerMovement.cs
@@ -1,27 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float forwardSpeed = 5f; // Vitesse de déplacement avant
-    public float turnSpeed = 2f; // Vitesse de rotation
-    public float maxSpeed = 10f; // Vitesse maximale
-    public float maxStraightSpeed = 7f; // Vitesse maximale en ligne droite
-    public float inertia = 0.95f; // Inertie de mouvement
+   
+    private bool canAccel = true;
 
-    public float currentTurnSpeed = 0f;
+    public float forwardSpeed = 1f; // Vitesse de déplacement avant
+    public float turnSpeed = 0.1f; // Vitesse de rotation
+    public float maxSpeed = 10f; // Vitesse maximale
+    public float maxStraightSpeed = 10f; // Vitesse maximale en ligne droite
+    public float inertia = 0.5f; // Inertie de mouvement
+    private float currentTurnSpeed = 0f;
+    private Vector3 currentVelocity; // Nouvelle vélocité
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         // Mouvement automatique vers l'avant
         transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
 
-        // Augmentation de la vitesse en ligne droite
-        if (Mathf.Abs(Input.GetAxis("Horizontal")) < 0.1f && forwardSpeed < maxStraightSpeed)
+        if (canAccel == true)
         {
-            forwardSpeed += Time.deltaTime;
+            // Augmentation de la vitesse en ligne droite
+            if (Mathf.Abs(Input.GetAxis("Horizontal")) < 0.1f && forwardSpeed < maxStraightSpeed)
+            {
+                forwardSpeed += Time.deltaTime * 8;
+            }
         }
+
 
         // Rotation de la moto
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -29,6 +43,7 @@ public class PlayerMovement : MonoBehaviour
 
         // Appliquer la rotation
         transform.Rotate(Vector3.up, currentTurnSpeed);
+        
 
         // Limiter la vitesse maximale
         Vector3 currentVelocity = GetComponent<Rigidbody>().velocity;
@@ -40,5 +55,23 @@ public class PlayerMovement : MonoBehaviour
 
         // Appliquer l'inertie
         GetComponent<Rigidbody>().velocity *= inertia;
+        
+        
+    }
+    
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        // Si la moto entre en collision avec un mur, arrête le mouvement
+        if (collision.gameObject.CompareTag("Mur"))
+        {
+            forwardSpeed = 10f;
+            canAccel = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        canAccel = true;
     }
 }
